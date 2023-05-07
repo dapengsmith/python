5be8f466f8bb54cf4610e08b919d25fe906bc2c9
dapengsmith <63896316+dapengsmith@users.noreply.github.com>
2023-05-07 11:29:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-with st.echo():
-    st.write('This code will be printed')
+with st.sidebar:
+    with st.echo():
+        st.write("This code will be printed to the sidebar.")
+
+    with st.spinner("Loading..."):
+        time.sleep(5)
+    st.success("Done!")
